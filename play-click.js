b0f2b2c390b7ed4052ef35cecee750690a1e2821
Vishmaya <72074398+VishmayaGithub@users.noreply.github.com>
2022-05-26 17:33:26 +0530

    Update play-click.js

--- play-click.js
+++ play-click.js
@@ -1,17 +1,35 @@
-/* global AFRAME */
 AFRAME.registerComponent("play-on-click", {
-    init: function() {
-      this.onClick = this.onClick.bind(this);
-    },
-    play: function() {
-      window.addEventListener("click", this.onClick);
-    },
-    onClick: function(evt) {
-      var videoEl = this.el.getAttribute("material").src;
-      if (!videoEl) {
-        return;
-      }
-      this.el.object3D.visible = true;
-      videoEl.play();
+  schema: {
+    isPlaying: { type: "boolean", default: false }
+  },
+
+  init: function() {
+    this.videoEl = this.el.getAttribute("material").src;
+    this.onClick = this.onClick.bind(this);    
+  },
+
+  play: function() {
+    window.addEventListener("click", this.onClick);
+  },
+  onClick: function(evt) {
+    if (!this.videoEl) {
+      return;
     }
-  });
+
+    var isPlaying = this.el.getAttribute("play-on-click").isPlaying;
+
+    this.el.object3D.visible = true;
+
+    if (!isPlaying) {
+      this.el.setAttribute("play-on-click", {
+        isPlaying: true
+      });
+      this.videoEl.play();
+    } else {
+      this.el.setAttribute("play-on-click", {
+        isPlaying: false
+      });
+      this.videoEl.pause();
+    }
+  }
+});
